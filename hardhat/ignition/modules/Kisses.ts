f4d919ec4dd793a5b153f48b4241699bd7123dd8
David Hurley <dhurls99@gmail.com>
2024-08-15 16:30:12 -0400

    try new contract

--- hardhat/ignition/modules/Kisses.ts
+++ hardhat/ignition/modules/Kisses.ts
@@ -2,7 +2,7 @@
 
 // const LockModule = buildModule("LockModule", (m) => {
 
-//   const lock = m.contract("Kisses", []);
+//   const lock = m.contract("Kisses", ['0x1169E27981BceEd47E590bB9E327b26529962bAe']);
 
 //   return { lock };
 // });

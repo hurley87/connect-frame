0d044397aed3c86619d18e3812703fb6ed5ac324
David Hurley <dhurls99@gmail.com>
2024-08-16 12:22:38 -0400

    add frame to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,17 @@
 import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from './config';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
-    },
-    {
-      action: 'tx',
-      label: 'Send Base Sepolia',
-      target: `${NEXT_PUBLIC_URL}/api/tx`,
-      postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
+      label: 'Play Kiss or Slap, Earn $Star Tokens',
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    src: `https://kissorslap.vercel.app/logo.jpg`,
     aspectRatio: '1:1',
   },
-  input: {
-    text: 'Tell me a story',
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+  postUrl: `https://kissorslap.vercel.app/api/connect`,
 });
 
 export const metadata: Metadata = {
@@ -30,7 +20,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Kiss or Slap',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    images: [`https://kissorslap.vercel.app/logo.jpg`],
   },
   other: {
     ...frameMetadata,
@@ -40,7 +30,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>kissorslap.xyz</h1>
+      <h1>Kiss or Slap</h1>
     </>
   );
 }
